8424fde75abb0d761a2596c287e2b77f8c7dcc33
hu81 <937419482@qq.com>
2019-09-01 21:19:29 +0800

    'ssr'

---
+++ layouts/default.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <!-- 头部组件 -->
+    <header />
+   <!-- 内容占位组件 代替了坑 -->
+    <nuxt />
+    <!-- 尾部组件 -->
+    <footer />
+  </div>
+</template>
+<script>
+import header from '@/compinents/header'
+import footer from '@/compinents/footer'
+export default {
+  // 注册组件
+  components:{
+    header,
+    footer
+  }
+}
+</script>
+<style lang='less'>
+html {
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*{
+  margin: 0;
+  padding: 0;
+}
+ul,li,ol{
+  list-style:none;
+}
+a{
+  text-decoration: none;
+  color:inherit       
+}
+em,i{
+  font-style: normal;
+}
+</style>
---
+++ pages/tab.vue
@@ -0,0 +1,63 @@
+<template>
+  <div style="margin-bottom:20px;padding:50px;">
+    <div class="tabs">
+      <span 
+      v-for="(item,index) in ['东','南','西','北']"
+      :key="index"
+      @click="handleClick(index)"
+      :class="{active:current === index}"
+      >{{item}} </span>
+    </div>
+
+    <div v-show="current == 0">
+      <ul>
+        <li>青</li>
+      </ul>
+    </div>
+
+    <div v-show="current == 1">
+      <ul>
+        <li>红</li>
+      </ul>
+    </div>
+
+    <div v-show="current == 2">
+      <ul>
+        <li>白</li>
+      </ul>
+    </div>
+
+    <div v-show="current == 3">
+      <ul>
+        <li>黑</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      current:0
+    }
+  },
+  methods:{
+    handleClick(index){
+      this.current = index
+    }
+  }
+};
+</script>
+
+<style lang='less' scoped>
+.tabs {
+  margin-bottom: 20px;
+  span {
+    cursor: pointer;   // css属性光标  // 一只手
+  }
+}
+.active{
+  color:red
+}
+</style>
